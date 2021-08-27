c3104704e2fc7069b39418710669f4024373adae
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-27 17:52:27 +0900

    スタイル適用

--- app/src/components/User.jsx
+++ app/src/components/User.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 300px;
+  margin: 0 auto;
+`
+
+const List = styled.li`
+  background-color: #fff;
+  list-style: none;
+  margin: 16px 0;
+  padding: 16px;
+  border-radius: 10px;
+  font-size: 16px;
+  box-shadow: 0 4px 2px #ccc;
+  & h3 {
+    text-align: center;
+    margin: 8px 0;
+  }
+  & p {
+    margin: 12px 0;
+  }
+  & span {
+    font-size: 18px;
+  }
+`
 
 export const User = (props) => {
 
   const { data } = props;
 
   return (
-    <ul>
-      <li>
-        <p>{`name : ${data.name}`}</p>
-        <p>{`nick name : ${data.username}`}</p>
-        <p>{`phone : ${data.phone}`}</p>
-        <p>{`email : ${data.email}`}</p>
-        <p>{`website : ${data.website}`}</p>
-      </li>
-    </ul>
+    <Container>
+      <ul>
+        <List>
+          <h3>{data.name}</h3>
+          <p>nick name : <span>{data.username}</span></p>
+          <p>phone : <span>{data.phone}</span></p>
+          <p>email : <span>{data.email}</span></p>
+          <p>website : <span>{data.website}</span></p>
+        </List>
+      </ul>
+    </Container>
   )
 }
--- app/src/components/Users.jsx
+++ app/src/components/Users.jsx
@@ -1,4 +1,32 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 300px;
+  margin: 0 auto;
+`
+
+const Header = styled.h2`
+  margin: 0 auto;
+  text-align: center;
+`
+
+const List = styled.li`
+  background-color: #fff;
+  text-align: center;
+  list-style: none;
+  margin: 16px 0;
+  padding: 16px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
+  box-shadow: 0 4px 2px #ccc;
+  &:hover {
+    font-size: 18px;
+    color: red;
+    font-weight: bold;
+  }
+`
 
 export const Users = (props) => {
   // console.log(props);
@@ -6,14 +34,17 @@ export const Users = (props) => {
   const { data, onClick} = props;
 
   return (
-    <ul>
-      {
-        data.map(val => (
-          <li key={val.id} onClick={onClick} id={val.id}>
-            <h4>{`name : ${val.name}`}</h4>
-          </li>
-        ))
-      }
-    </ul>
+    <Container>
+      <ul>
+        <Header>User Name List</Header>
+        {
+          data.map(val => (
+            <List key={val.id} onClick={onClick} id={val.id}>
+              <p>{`${val.name}`}</p>
+            </List>
+          ))
+        }
+      </ul>
+    </Container>
   )
 }
